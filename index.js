d040de2030b2d1b78ca703ae37fc71199c56bad3
CaiLI <dy5025115@gmail.com>
2020-03-15 19:34:48 +0800

    Added eslint and fix format

--- index.js
+++ index.js
@@ -7,25 +7,24 @@ mongoose.connect('mongodb://localhost:27017/course', {
     console.log('MongoDB connected...');
   });
 
-  const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    date: {type: Date, default: Date.now},
-    isPublished: Boolean
-  });
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
   // Model
   // An instance of a model is called a document. 底層MongoDB數據庫創建和讀取文檔
-  const Course = mongoose.model('Course', courseSchema);  // Course is Model instance.
-  async function createData () {
-    const course = new Course({
-      name: 'Chinese',
-      auther: 'Mary',
-      tags: ['language'],
-      isPublished: false
-    })
-    const rlt = await course.save();
-    console.log('MongoDB has created: ', rlt);
-  }
-  // const MyModel = mongoose.model('ModelName', mySchema);
-  createData();
+const Course = mongoose.model('Course', courseSchema); // Course is Model instance.
+async function createData () {
+  const course = new Course({
+    name: 'Chinese',
+    auther: 'Mary',
+    tags: ['language'],
+    isPublished: false
+  });
+  const rlt = await course.save();
+  console.log('MongoDB has created: ', rlt);
+}
+createData();
